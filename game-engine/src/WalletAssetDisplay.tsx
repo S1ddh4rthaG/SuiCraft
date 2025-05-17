cfe638bb50fd91101fbe6567b788096a2bcd9e74
Siddhartha G <cs20b040@iittp.ac.in>
2025-05-17 23:06:10 +0530

    Basic

--- game-engine/src/WalletAssetDisplay.tsx
+++ game-engine/src/WalletAssetDisplay.tsx
@@ -19,8 +19,8 @@ function WalletAssetDisplay() {
       <div className='w-100 text-center standard-background px-2 py-2 mb-1'>
         <Wallet />
       </div>
-      {/* {isConnected && <AssetUploadAndFetch />} */}
-      {isConnected && <WalrusAssetHandler />}
+      {isConnected && <AssetUploadAndFetch />}
+      {/* {isConnected && <WalrusAssetHandler />} */}
     </>
   )
 }
